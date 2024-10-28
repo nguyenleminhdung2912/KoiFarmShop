7d36b554706dfe05b1367c4eaa1140dfb9a00f5d
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2024-10-28 19:36:48 +0700

    Complete CRUD product for staff

--- DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
+++ DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365783d9ba92448a9528d36769b8d4de421a9355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72503bce14917e4be81faa5a1b3d2d8c272b6be3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
