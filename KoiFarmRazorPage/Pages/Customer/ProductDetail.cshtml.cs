cd1f54ea8ec33315b6b7331aeab239fe3cc97c60
HaiNTDev <ccutcnez@gmail.com>
2024-11-11 19:06:05 +0700

    Update thanh toan

--- KoiFarmRazorPage/Pages/Customer/ProductDetail.cshtml.cs
+++ KoiFarmRazorPage/Pages/Customer/ProductDetail.cshtml.cs
@@ -13,9 +13,11 @@ public class ProductDetail : PageModel
     private readonly IProductRepository _productRepository;
     private readonly ICartRepository _cartRepository;
 
-    public ProductDetail(IProductRepository productRepository)
+
+    public ProductDetail(IProductRepository productRepository, ICartRepository cartRepository)
     {
-        this._productRepository = productRepository;
+        _productRepository = productRepository;
+        _cartRepository = cartRepository;  // Add this injection
     }
 
     public void OnGet(long id)
@@ -23,19 +25,15 @@ public class ProductDetail : PageModel
         Product = _productRepository.GetProductById(id);
     }
 
-    public void OnGet(int productId)
-    {
-        Product = _productRepository.GetProductById(productId); // Load product using ProductRepository
-    }
     public IActionResult OnPostAddToCart( long productId, int quantity)
     {
         var product = _productRepository.GetProductById(productId);
 
         if (product != null)
         {
-            _cartRepository.AddProduct(product, quantity);
+            _cartRepository.AddProduct(product, 1);
         }
-
-        return Page(); 
+       
+        return RedirectToPage("/Customer/ProductDetail", new {id = productId});
     }
 }
